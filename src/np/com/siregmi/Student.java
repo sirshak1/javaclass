34e4817213581c50fe62cda1fb47c7bfef72bcea
Sirshak Regmi <barcaregmi@gmail.com>
2019-03-18 18:57:26 -0700

    added changes

---
+++ src/np/com/siregmi/Student.java
@@ -0,0 +1,14 @@
+package np.com.siregmi;
+
+public class Student {
+    public void printRollNo()
+    {
+        int rollno =1;
+        System.out.println(rollno);
+    }
+    public void printName()
+    {
+        int name =1;
+        System.out.print(name);
+    }
+}
---
+++ src/np/com/siregmi/main.java
@@ -0,0 +1,14 @@
+package np.com.siregmi;
+
+public class main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.printRollNo();
+        student.printName();
+    }
+}
+
+
+
+
